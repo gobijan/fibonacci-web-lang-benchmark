b27549eee415feafe4061ac64f6897069a256677
Frank Fischer <fischer@intertech.de>
2015-11-04 11:35:22 +0100

    Add jruby/sinatra on torquebox 4 version

---
+++ jruby/app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+set :logging, false
+
+def fib(n)
+  if n == 0
+    return 0
+  elsif n == 1
+    return 1
+  else
+    return fib(n-1) + fib(n-2)
+  end
+end
+
+get '/:number' do
+  fib(params[:number].to_i).to_s
+end
